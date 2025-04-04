9a09931f3f736f32c0643732b01674646929ef1e
jota <j.pedro1415@ufu.br>
2025-04-04 15:14:20 -0300

    bd

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'localhost',
-  user: 'jota',       // Substitua pelo seu usuário
-  password: '293121',   // Substitua pela sua senha
-  database: 'meu_site_db',   // Nome do banco de dados
-  port: 5432,
+  host: process.env.DB_HOST,       // Ex: ec2-xx-xx-xx-xx.compute-1.amazonaws.com
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 5432,
+  ssl: { rejectUnauthorized: false } // Necessário para conexões seguras, especialmente no Heroku
 });
 
 module.exports = pool;
